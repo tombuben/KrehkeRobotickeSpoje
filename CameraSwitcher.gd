b17aad7bd89bf90e7275394a1d582b1215eb839b
Jakub Hubáček <hubacek.jakub@gmail.com>
2024-03-15 21:05:58 +0100

    camera switching to storyboard

--- CameraSwitcher.gd
+++ CameraSwitcher.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var gameplayCamera: Camera2D
-@onready var storyboardCamera : Camera2D = $StoryboardCamera
+@onready var storyboardCamera : storyboardCamera = $StoryboardCamera
 
 @export var storyboards: Array[Sprite2D]
 
@@ -9,6 +9,7 @@ var counter: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	storyboardCamera.
 	pass # Replace with function body.
 
 func _input(event):
@@ -16,9 +17,16 @@ func _input(event):
 		switchCamera(counter)
 		counter = counter+1
 		
+		
+func activateStoryboard(storyboardIndex: int):
+	if storyboards[storyboardIndex] != null:
+		var storyboard = storyboards[storyboardIndex]
+		storyboardCamera.activate(storyboard)
+		
 
 func switchCamera(storyboardIndex: int):
 	if storyboards[storyboardIndex] != null:
 		var storyboard = storyboards[storyboardIndex]
 		storyboardCamera.global_position = storyboard.global_position
+		
 		storyboardCamera.make_current()
