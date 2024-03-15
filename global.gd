c74d1d8025801e4e6b269345090dae77bbdc3617
tomas bubenicek <tomas.bubenicek@flying-rat.online>
2024-03-15 21:19:54 +0100

    added konec

--- global.gd
+++ global.gd
@@ -2,6 +2,8 @@ extends Node
 
 var level : int = 1
 
+var MAX_LVL_COUNT = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,4 +14,5 @@ func _process(delta):
 	
 func progress_level():
 	level = level + 1
-	get_tree().change_scene_to_file("res://level_" + str(level) + ".tscn")
+	if !get_tree().change_scene_to_file("res://level_" + str(level) + ".tscn"):
+		get_tree().change_scene_to_file("res://konec.tscn")
