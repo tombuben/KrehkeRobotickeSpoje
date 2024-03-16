d7dccd6a04c410671b9b4a01b7f1669e19e6f8fb
tomas bubenicek <tomas.bubenicek@flying-rat.online>
2024-03-16 16:21:31 +0100

    ignore input for 1s on launch

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+var listening_to_input : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	await get_tree().create_timer(1).timeout
+	listening_to_input = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,5 +13,5 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if !event is InputEventMouseMotion and !event is InputEventJoypadMotion:
+	if listening_to_input and !event is InputEventMouseMotion and !event is InputEventJoypadMotion:
 		get_tree().change_scene_to_file("res://IntroVideo.tscn")
