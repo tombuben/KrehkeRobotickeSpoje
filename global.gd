bd8fbda8968c004c333e81cc5aee062d093a50ed
tomas bubenicek <tomas.bubenicek@flying-rat.online>
2024-03-16 13:21:26 +0100

    esc na zavreni hry

--- global.gd
+++ global.gd
@@ -7,11 +7,12 @@ var LEVEL_CUTSCENE_FINISHED : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 	
 func progress_level():
 	level = level + 1
